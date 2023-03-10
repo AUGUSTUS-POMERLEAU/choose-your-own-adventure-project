d84600446b93fba17e06c467b01da79cd9500279
AUGUSTUS-POMERLEAU <apomerle@macalester.edu>
2023-03-10 16:19:50 -0600

    v1

--- dialogueTest.py
+++ dialogueTest.py
@@ -19,24 +19,32 @@ def divide_blocks(file):
 
 def normal_text(line):
     b = input(line)
+
+def get_level(line):
+    i = 0
+    for char in [x for x in line if x == "\t"]:
+        i += 1
+    return i
+
 def evaluateBlock(block_key):
     blocktext = blocks[block_key]
     queue_.remove(block_key)
     index = 0
+    currentLevel = 0
     while index < len(blocktext):
-        if blocktext[index][:3] == "\t->":
-            choices = [x for x in blocktext if x[:3] == "\t->"]
+        if "->" in blocktext[index]:
+            choices = [x for x in blocktext if ("->" in x)]
             n = 1
             for i in choices:
                 strOutput = str(choices.index(i) + 1)+ ": "+ i[2:].strip()
                 print(strOutput)
             choice_int = int(input("Put in an int"))
             index = blocktext.index(choices[choice_int - 1]) + 1
-        elif blocktext[index][0:3] == "\t\t#":
+        elif blocktext[index][0:2] == "\t#":
             queue_.append(blocktext[index].split("#")[1])
             break
         else:
-            b = input(blocktext[index])
+            b = input(blocktext[index].strip())
             index += 1
 
 with open("TextFiles/new.txt") as file:
